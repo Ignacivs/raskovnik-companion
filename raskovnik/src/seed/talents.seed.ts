02254b4912d41c5de9f4218c7526ee73962b643f
Oggy <ognjen.t@yandex.com>
2021-10-24 19:12:32 +0200

    Removed redundant files

---
+++ raskovnik/src/seed/talents.seed.ts
@@ -0,0 +1,34 @@
+export const talentsSeed = [
+  {
+    name: 'Jurisnik',
+    description: 'Jurisas test test.',
+    requerments: '${new Date()}',
+    type: 'Charging',
+    actionCost: '2'
+  },
+  {
+    name: 'Jurisnik 2',
+    description: 'Jurisas test test.',
+    requerments: '${new Date()}',
+    type: 'Charging',
+    actionCost: '2'
+  },{
+    name: 'Jurisnik 3',
+    description: 'Jurisas test test.',
+    requerments: '${new Date()}',
+    type: 'Charging',
+    actionCost: '2'
+  },{
+    name: 'Jurisnik 4',
+    description: 'Jurisas test test.',
+    requerments: '${new Date()}',
+    type: 'Charging',
+    actionCost: '2'
+  },{
+    name: 'Jurisnik 5',
+    description: 'Jurisas test test.',
+    requerments: '${new Date()}',
+    type: 'Charging',
+    actionCost: '2'
+  }
+];
